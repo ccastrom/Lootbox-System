6d7943e6cb1972798894bf08c5b775e3982f5162
ccastrom <claud@LAPTOP-V9QC7M78>
2018-05-16 18:40:43 -0400

    Principal structured software are translated, need to change the language the items name and the quality name.

--- Lootbox-system/Lootbox/src/lootbox/Lootbox.java
+++ Lootbox-system/Lootbox/src/lootbox/Lootbox.java
@@ -6,10 +6,10 @@ import java.util.Random;
 
 public class Lootbox {
 
-    public static int contcomun = 0;
-    public static int contraro = 0;
+    public static int contcommon = 0;
+    public static int contrare = 0;
     public static int contleg = 0;
-    public static int contsobres=0;
+    public static int contalphapacks=0;
     public static String random;
 
     public static void main(String[] args) {
@@ -27,34 +27,34 @@ public class Lootbox {
             System.out.println("1.- Open alpha pack");
             System.out.println("2.- Exit");
             System.out.println("3.- Stats");
-            if(contsobres>0){
-                System.out.println("Last object claim: "+random);
+            if(contalphapacks>0){
+                System.out.println("Last object claimed: "+random);
             }
             System.out.println("------------------------");
             op = input.nextInt();
             if (op == 1) {
                 System.out.println("================");
-                System.out.println("1.-Abrir sobre");
+                System.out.println("1.-Open alpha pack");
                 System.out.println("================");
-                carga();
+                loader();
             }
 
             if (op == 2) {
-                System.out.println("Fin del programa");
+                System.out.println("end of the software");
                 break;
             }
             if (op == 3) {
-                System.out.println("Estadisticas");
+                System.out.println("Stats");
                 estadistica();
 
                 System.out.println();
 
             }
             if (op < 1) {
-                System.out.println("Opcion no valida");
+                System.out.println("Invalid option");
             }
             if (op > 3) {
-                System.out.println("Opcion no valida");
+                System.out.println("invalid option");
             }
             
 
@@ -62,44 +62,44 @@ public class Lootbox {
 
     }
 
-    public static void carga() {
+    public static void loader() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Cargando sobre");
-        System.out.println("Espere.");
+        System.out.println("loading alpha pack");
+        System.out.println("Wait please.");
         try {
             Thread.sleep(1200);
 
         } catch (InterruptedException ex) {
 
         }
-        System.out.println("Espere..");
+        System.out.println("Wait..");
         try {
             Thread.sleep(1200);
         } catch (InterruptedException ex) {
 
         }
-        System.out.println("Espere...");
+        System.out.println("Wait...");
         try {
             Thread.sleep(1200);
         } catch (InterruptedException ex) {
         }
-        System.out.println("¡SOBRE CARGADO!");
+        System.out.println("¡ALPHA PACK LOADED!");
         System.out.println("===================");
         String enter = input.nextLine();
         if (enter.equals("")) {
-            System.out.println("¡ABRIENDO SOBRE!");
-            lista();
+            System.out.println("¡OPENING ALPHA PACK!");
+            list();
 
         }
         
 
     }
 
-    public static void lista() {
+    public static void list() {
 
-        String[] Objetocomun = {"Aluminio", "carton", "papel lustre"};
-        String[] Objetoraro = {"\033[35mCobre", "\033[35mPlata", "\033[35mBronze", "\033[35mTitanio"};
-        String[] Objetolegendario = {"\033[33mOro", "\033[33mPlatino", "\033[33mDiamante", "\033[33mRubi"};
+        String[] commonobject = {"Aluminio", "carton", "papel lustre"};
+        String[] rareobject = {"\033[35mCobre", "\033[35mPlata", "\033[35mBronze", "\033[35mTitanio"};
+        String[] legendaryobject = {"\033[33mOro", "\033[33mPlatino", "\033[33mDiamante", "\033[33mRubi"};
 
 //        int listacomun = 60;
 //        int listararo = 30;
@@ -108,37 +108,37 @@ public class Lootbox {
         int total = porcentaje.nextInt(101);
         if (total > 0 && total <= 60) {
             System.out.println("comun");
-             random= (Objetocomun[new Random().nextInt(Objetocomun.length)]);
+             random= (commonobject[new Random().nextInt(commonobject.length)]);
             System.out.println(random + "*OO");
-            contcomun++;
-            contsobres++;
+            contcommon++;
+            contalphapacks++;
 
         }
         if (total > 60 && total <= 89) {
-             random= (Objetoraro[new Random().nextInt(Objetoraro.length)]);
+             random= (rareobject[new Random().nextInt(rareobject.length)]);
             System.out.println("\033[35mRaro");
             System.out.println(random + "**O");
-            contraro++;
-            contsobres++;
+            contrare++;
+            contalphapacks++;
 
         }
         if (total > 89 && total <= 100) {
             System.out.println("");
             System.out.println("\033[33m¡Legendario!");
-             random= (Objetolegendario[new Random().nextInt(Objetolegendario.length)]);
+             random= (legendaryobject[new Random().nextInt(legendaryobject.length)]);
             System.out.println(random + "***");
             contleg++;
-            contsobres++;
+            contalphapacks++;
 
         }
 
     }
 
     public static void estadistica() {
-        System.out.println("Sobres abiertos: "+contsobres);
-        System.out.println("Objetos comunes obtenidos: " + contcomun);
-        System.out.println("Objetos raros obtenidos: " + contraro);
-        System.out.println("Objetos legendarios obtenidos: " + contleg);
+        System.out.println("Alpha packs openeds: "+contalphapacks);
+        System.out.println("Common objects obtained: " + contcommon);
+        System.out.println("Rares objects obtained: " + contrare);
+        System.out.println("Legendarys objects obtained: " + contleg);
 
     }
 
